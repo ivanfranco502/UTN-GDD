38d99d073e05578c30006257ba0101584e4c9c2a
Iván Franco <22622318+ivanfranco502@users.noreply.github.com>
2011-09-27 14:27:48 +0000

    Puesta en marcha

---
+++ Aplicacion Desktop/BoletoElectronicoDesktop/AbmPost-Net/FormAbmPostnet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BoletoElectronicoDesktop.AbmPost_Net
+{
+    public partial class FormAbmPostnet : Form
+    {
+        public FormAbmPostnet()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Base de Datos/gd_esquema.Schema.NoTeVaConsultar.sql
@@ -0,0 +1,5 @@
+USE [GD2C2011]
+GO
+/****** Object:  Schema [gd_eschema]    Script Date: 22/09/2011 13:27:26 ******/
+CREATE SCHEMA [NoTeVaConsultar] AUTHORIZATION [gd]
+GO
