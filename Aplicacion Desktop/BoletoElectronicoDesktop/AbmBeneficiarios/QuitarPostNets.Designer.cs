aac100742ffc12e364ee653daaeeeb89c89c1299
1370935.Franco.Ivan <1370935.Franco.Ivan@a0f9b5e9-09d6-434c-a853-bdac83c1b39c>
2011-11-08 17:05:47 +0000

    Programa listo Script listo Estrategia.docx actualizado

--- Aplicacion Desktop/BoletoElectronicoDesktop/AbmBeneficiarios/QuitarPostNets.Designer.cs
+++ Aplicacion Desktop/BoletoElectronicoDesktop/AbmBeneficiarios/QuitarPostNets.Designer.cs
@@ -57,6 +57,7 @@ namespace BoletoElectronicoDesktop.AbmBeneficiarios
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(416, 126);
             this.dataGridView1.TabIndex = 62;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label1
             // 
